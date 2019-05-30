643b1d64b224fcf7f1b03cebddb74330b6b1fe2d
jcork2 <mcconnell1214@gmail.com>
2019-05-29 21:33:45 -0500

    add generic combatant

---
+++ Games.Ann.Models/Combatants/GenericCombatant.cs
@@ -0,0 +1,14 @@
+﻿namespace Games.Ann.Models.Combatants
+{
+    public class GenericCombatant
+    {
+        public const int MaxHealth = 200;
+
+        private int _currentHealth;
+
+        public GenericCombatant()
+        {
+            _currentHealth = MaxHealth;
+        }
+    }
+}
